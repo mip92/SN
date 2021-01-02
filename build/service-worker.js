abdeabcf44a8ed7a7a1438ba7154baa7cf880ad1
mip92 <19mip92@gmail.com>
2021-01-02 17:05:13 +0200

    add

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mip92/SN/precache-manifest.9d16213fae6ab49d3e7c199ce94e219c.js"
+  "/mip92/SN/precache-manifest.7e98201bd8f57ac1ea8512efe2a25910.js"
 );
 
 self.addEventListener('message', (event) => {
